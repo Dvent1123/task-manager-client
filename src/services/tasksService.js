c93f52108b2eb7285db25fac5b6befe5bc08773b
Dvent1123 <ventura.daniel36@gmail.com>
2021-04-26 16:00:19 -0700

    new routes

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getAllTasks = async (token) => {
-    let res = await axios.get(`${process.env.REACT_APP_API}/tasks`, {
+    let res = await axios.get(`${process.env.REACT_APP_API}/api/tasks`, {
         headers: { Authorization: `Bearer ${token}`}
     })
     return res.data || []
--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getAllUsers = async (token) => {
-    let res = await axios.get(`${process.env.REACT_APP_API}/users`, {
+    let res = await axios.get(`${process.env.REACT_APP_API}/api/users`, {
       headers: { Authorization: `Bearer ${token}`}  
     })
     return res.data || []
