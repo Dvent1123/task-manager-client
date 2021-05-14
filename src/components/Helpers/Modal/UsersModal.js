784797116691b1f348a77e374e5b3a5ad5703e05
Brehtyn <66739729+Brehtyn@users.noreply.github.com>
2021-05-13 18:20:45 -0700

    bugfixes + headerbackground
    +included black headerbg
    +border on logo
    +button resizing in some modal forms
    +fixing header leak on some sizing

--- src/components/Helpers/Modal/UsersModal.js
+++ src/components/Helpers/Modal/UsersModal.js
@@ -37,7 +37,7 @@ const UsersModal = ({isShowing, hide, onSubmit,
                             </select>    
                         </div>        
                         <div className="form-group">
-                            <button className="task-button" type="submit">
+                            <button className="task-button" id="modal-form-button" type="submit">
                             Submit
                             </button>
                         </div>
